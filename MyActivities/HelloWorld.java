98f34efb615b27f0a7bc6b5969524a8387bfdfce
Rhaw89 <159610180+Rhaw89@users.noreply.github.com>
2024-04-18 07:13:49 +0000

    length, chartAt, indexOf, substring, toUpperCase, toLowerCase

---
+++ MyActivities/HelloWorld.java
@@ -0,0 +1,18 @@
+package MyActivities;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        String s1 = "ani mals";
+        
+        System.out.println(s1.length());
+       // System.out.println(s1.size());
+        System.out.println(s1.charAt(1));
+        System.out.println(s1.indexOf("n"));
+        System.out.println(s1.indexOf("s"));
+        System.out.println(s1.substring(2,6));
+        System.out.println(s1.toUpperCase());
+        System.out.println(s1.toLowerCase());
+        System.out.println(s1);
+    
+    }
+}
--- MyActivities/SampleArray.java
+++ MyActivities/SampleArray.java
@@ -2,11 +2,16 @@ package MyActivities;
 
 public class SampleArray {
     public static void main(String[] args) {
-        for (int i = 1; i <= 4; i++) {
+      /*   for (int i = 1; i <= 4; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print(i);
             }
             System.out.println();
         }
+        */
+
+
+
+        
     }
 }
